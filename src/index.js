97088d73abc5202b150ee6019b130ec8d527e813
ANDRES FELIPE VILLAMIZAR PALACIO <AndresF.VillamizarP@ugcbogota.edu.co>
2022-02-10 15:12:07 -0500

    Portafolio mejorado

---
+++ src/index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    $("#title").attr("style", "color: blue")
+    $("#divDePrueba").attr("style", "color: blue")
+
+    console.log("Funcionando");
+
+    //document.getElementById("divDePrueba").innerHTML = "My First JavaScript";
+}
